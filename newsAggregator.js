1df90831cd52b364f06bfcf4b75efaf2d510ba25
Rishabh Sundli <rishabhsundli024@gmail.com>
2025-10-17 16:58:59 +0530

    4 sources added and fetching

--- newsAggregator.js
+++ newsAggregator.js
@@ -1,4 +1,5 @@
 function fetchSourceA(){
+
     let random=Math.floor(Math.random()*10);
     return new Promise((resolve,reject)=>{
  setTimeout(()=>{
@@ -24,4 +25,33 @@ function fetchSourceB(){
     })
 }
 
+function fetchSourceC(){
+    let random=Math.floor(Math.random()*10);
+    return new Promise((resolve,reject)=>{
+ setTimeout(()=>{
+              if(random>4){
+            resolve({title:"Gaming news",source:"Blogs"});
+           }else{
+            reject(`401 Unauthorized`);
+           }
+        },Math.floor(Math.random()*1600))
+    })
+}
+
+function fetchSourceD(){
+    let random=Math.floor(Math.random()*10);
+    return new Promise((resolve,reject)=>{
+ setTimeout(()=>{
+              if(random>4){
+            resolve({title:"Gaming news",source:"Podcast"});
+           }else{
+            reject(`403 Forbidden`);
+           }
+        },Math.floor(Math.random()*1800))
+    })
+}
+
 fetchSourceA().then(n=>console.log(n)).catch(n=>console.log(n))
+fetchSourceB().then(n=>console.log(n)).catch(n=>console.log(n))
+fetchSourceC().then(n=>console.log(n)).catch(n=>console.log(n))
+fetchSourceD().then(n=>console.log(n)).catch(n=>console.log(n))
