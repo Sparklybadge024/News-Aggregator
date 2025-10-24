03b8d4c2013a52e9287ec7cda77143eec3b469c8
Rishabh Sundli <rishabhsundli024@gmail.com>
2025-10-24 15:11:13 +0530

    sequential function completed and sound

--- newsAggregator.js
+++ newsAggregator.js
@@ -1,3 +1,4 @@
+// Step 1:-
 function fetchSourceA(){
 
     let random=Math.floor(Math.random()*10);
@@ -53,18 +54,21 @@ function fetchSourceD(){
 
 
 
+// Step 2:-
 
 function aggeregateSequential(){
 return fetchSourceA()
-.then(n=>{console.log(`Source A Resolved `,n); return fetchSourceB();})
-.catch(i=>{console.log(`Source A Rejected `,i); return fetchSourceB();})
-.then(a=>{console.log(`Source B Resolved `,a);return fetchSourceC()})
-.catch(b=>{console.log(`Source B Rejected `,b);return fetchSourceC()})
-.then(c=>{console.log(`Source C Resolved `,c);return fetchSourceD()})
-.catch(d=>{console.log(`Source C Rejected `,d);return fetchSourceD()})
-.then(e=>console.log(`Source D Resolved `,e))
-.catch(f=>console.log(`Source D Rejected `,f))
+.then(n=>{console.log(`Source A Resolved`,n); return fetchSourceB();},
+      i=>{console.log(`Source A Rejected`,i); return fetchSourceB()})
+.then(a=>{console.log(`Source B Resolved`,a); return fetchSourceC()},
+      b=>{console.log(`Source B Rejected`,b); return fetchSourceC()})
+.then(c=>{console.log(`Source C Resolved`,c); return fetchSourceD()},
+      d=>{console.log(`Source C Rejected`,d); return fetchSourceD()})
+.then(e=>{console.log(`Source D Resolved`,e)},
+      f=>{console.log(`Source D Resolved`,f)})
+
 
 }
 
 aggeregateSequential()
+
